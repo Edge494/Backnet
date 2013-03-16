699d1550f732cdb4acbe9c32eee48a953496a96a
Edmond van der Plas <Edge@iMac-van-Edmond-van-der-Plas.local>
2013-03-16 11:26:04 +0100

    Initial Commit

---
+++ Backnet/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Backnet
+//
+//  Created by Edmond van der Plas on 16-03-13.
+//  Copyright (c) 2013 Edmond van der Plas. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Backnet/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Backnet
+//
+//  Created by Edmond van der Plas on 16-03-13.
+//  Copyright (c) 2013 Edmond van der Plas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
